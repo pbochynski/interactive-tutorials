69fe6eb5960abcbe884d6d9db9dbac091a563c01
pbochynski <p.bochynski@gmail.com>
2023-06-06 16:42:52 +0200

    Update path

--- rate-limit/init.sh
+++ rate-limit/init.sh
@@ -1,7 +1,8 @@
 kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.17.2 sh -
-export PATH=$PWD/istio-1.17.2/bin:$PATH
-istioctl install -f deployments/istio-profile.yaml -y
+cd istio-1.17.2
+export PATH=$PWD/bin:$PATH
+istioctl install -f ~/deployments/istio-profile.yaml -y
 kubectl label namespace default istio-injection=enabled
 kubectl apply -f samples/httpbin/httpbin.yaml 
 kubectl apply -f samples/httpbin/httpbin-gateway.yaml 
