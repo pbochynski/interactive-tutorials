570ba4935d93187f8d8607909d26b6d867b3e3d9
pbochynski <p.bochynski@gmail.com>
2023-06-06 16:35:45 +0200

    Istio profile

--- rate-limit/init.sh
+++ rate-limit/init.sh
@@ -2,7 +2,7 @@ kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedul
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.17.2 sh -
 cd istio-1.17.2
 export PATH=$PWD/bin:$PATH
-istioctl install --set profile=demo -y
+istioctl install -f deployments/istio-profile.yaml -y
 kubectl label namespace default istio-injection=enabled
 kubectl apply -f samples/httpbin/httpbin.yaml 
 kubectl apply -f samples/httpbin/httpbin-gateway.yaml 
