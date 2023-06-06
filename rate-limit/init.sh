1a65761056a7a87b8d57af6147e4b9c6abbdf30f
pbochynski <p.bochynski@gmail.com>
2023-06-06 11:44:00 +0200

    Disable init commands

--- rate-limit/init.sh
+++ rate-limit/init.sh
@@ -1,9 +1,9 @@
 kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
-curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.17.2 sh -
-cd istio-1.17.2
-export PATH=$PWD/bin:$PATH
-istioctl install --set profile=demo -y
-kubectl label namespace default istio-injection=enabled
-kubectl apply -f samples/httpbin/httpbin.yaml 
-kubectl apply -f samples/httpbin/httpbin-gateway.yaml 
+# curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.17.2 sh -
+# cd istio-1.17.2
+# export PATH=$PWD/bin:$PATH
+# istioctl install --set profile=demo -y
+# kubectl label namespace default istio-injection=enabled
+# kubectl apply -f samples/httpbin/httpbin.yaml 
+# kubectl apply -f samples/httpbin/httpbin-gateway.yaml 
 echo 'Prerequisites are already installed. You can move to the next page'
