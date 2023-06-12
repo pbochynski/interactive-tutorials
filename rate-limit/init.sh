f407fe79efc0966f884adda4bc1c6a7b0056b1e1
pbochynski <p.bochynski@gmail.com>
2023-06-12 15:10:12 +0200

    Update to istio 1.18.0

--- rate-limit/init.sh
+++ rate-limit/init.sh
@@ -1,6 +1,6 @@
 kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
-curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.17.2 sh -
-cd istio-1.17.2
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.18.0 sh -
+cd istio-1.18.0
 export PATH=$PWD/bin:$PATH
 cd ~/deployments
 istioctl install -f ~/deployments/istio-profile.yaml -y
