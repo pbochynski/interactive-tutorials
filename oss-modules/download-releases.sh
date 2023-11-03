12e4ee536a347174af3cd341636a155af3ed9235
pbochynski <p.bochynski@gmail.com>
2023-11-03 13:02:30 +0100

    OSS modules

--- oss-modules/download-releases.sh
+++ oss-modules/download-releases.sh
@@ -1,4 +1,4 @@
 mkdir -p public/modules
 kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
 curl -L https://github.com/kyma-project/serverless-manager/releases/latest/download/serverless-operator.yaml -o public/modules/serverless-operator.yaml
-kubectl proxy -w='.' -P='/public/' &
+kubectl proxy -w='public' -P='/public/' &
