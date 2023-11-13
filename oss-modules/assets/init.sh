3993a89244619f12901a97d265e685fde148dcf2
pbochynski <p.bochynski@gmail.com>
2023-11-13 22:37:14 +0100

    OSS modules

--- oss-modules/assets/init.sh
+++ oss-modules/assets/init.sh
@@ -1,6 +1,5 @@
 kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl create namespace kyma-system
 curl https://kyma-project.github.io/community-modules/kyma.html -o kyma.html
-curl https://kyma-project.github.io/community-modules/app.js -o app.js
 
 kubectl proxy -w='.' --address='0.0.0.0' --accept-hosts='.*' &
