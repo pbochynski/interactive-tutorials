ceab7506b768920fba4e6402047c82a33c5445f2
pbochynski <p.bochynski@gmail.com>
2023-11-17 21:39:19 +0100

    Use busola.tar.gz

--- oss-modules/assets/init.sh
+++ oss-modules/assets/init.sh
@@ -1,7 +1,5 @@
 kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl create namespace kyma-system
-container_id=$(docker create europe-docker.pkg.dev/kyma-project/prod/busola:latest)
-docker cp $container_id:/app/core-ui ./app
-curl https://kyma-project.github.io/community-modules/kyma.html -o app/kyma.html
-
-kubectl proxy --api-prefix='/backend/' -w='app' --www-prefix='' --address='0.0.0.0' --accept-hosts='.*' &
+curl https://kyma-project.github.io/community-modules/busola.tar.gz -o busola.tar.gz
+tar -xf busola.tar.gz 
+kubectl proxy --api-prefix='/backend/' -w='busola' --www-prefix='' --address='0.0.0.0' --accept-hosts='.*' &
