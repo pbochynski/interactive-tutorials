a1bf23682ec9500d94687f3caa1325d0530e5adc
pbochynski <p.bochynski@gmail.com>
2024-01-04 14:36:19 +0100

    Update to kyma cli

--- oss-modules/assets/init.sh
+++ oss-modules/assets/init.sh
@@ -1,6 +1,8 @@
 kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl create namespace kyma-system
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source .bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 21.5
 npm install -g kyma
