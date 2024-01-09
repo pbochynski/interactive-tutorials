524aa09966d0bb31e7110d60ab1464b5bb3e2799
pbochynski <p.bochynski@gmail.com>
2024-01-09 12:38:27 +0100

    Change node version

--- oss-modules/assets/init.sh
+++ oss-modules/assets/init.sh
@@ -4,5 +4,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 21.5
+nvm install 20.10
 npm install -g kyma
