d6f84e391b2d8755b0ec6bbc0ab86c0b912cc5cc
pbochynski <p.bochynski@gmail.com>
2025-04-29 12:39:41 +0200

    Upgrade btp cli

--- btp-services/init.sh
+++ btp-services/init.sh
@@ -1,4 +1,4 @@
-curl -s -0L -H 'cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt' https://tools.hana.ondemand.com/additional/btp-cli-linux-amd64-2.83.0.tar.gz -o btp-cli.tar.gz
+curl -s -0L -H 'cookie: eula_3_2_agreed=tools.hana.ondemand.com/developer-license-3_2.txt' https://tools.hana.ondemand.com/additional/btp-cli-linux-amd64-2.83.0.tar.gz -o btp-cli.tar.gz
 tar -vxzf btp-cli.tar.gz
 mv linux-amd64/btp /usr/local/bin
 kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
