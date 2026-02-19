63dcc84f1d376f0630b13da5aeaea8fea26de501
pbochynski <p.bochynski@gmail.com>
2026-02-19 13:05:59 +0100

    Update btp cli

--- btp-services/init.sh
+++ btp-services/init.sh
@@ -1,5 +1,5 @@
 # Install BTP CLI
-curl -s -0L -H 'cookie: eula_3_2_agreed=tools.hana.ondemand.com/developer-license-3_2.txt' https://tools.hana.ondemand.com/additional/btp-cli-linux-amd64-2.83.0.tar.gz -o btp-cli.tar.gz
+curl -s -0L -H 'cookie: eula_3_2_agreed=tools.hana.ondemand.com/developer-license-3_2.txt' https://tools.hana.ondemand.com/additional/btp-cli-linux-amd64-2.97.0.tar.gz -o btp-cli.tar.gz
 tar -vxzf btp-cli.tar.gz
 mv linux-amd64/btp /usr/local/bin
 # Install AWS CLI
